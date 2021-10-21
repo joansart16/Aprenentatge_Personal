0a41c7379505a88d53b7c58dec15dafd310d3c38
joansart16 <61794442+joansart16@users.noreply.github.com>
2021-10-21 15:55:39 +0200

    practica_4

--- 04_Multiclass_Classification/main.py
+++ 04_Multiclass_Classification/main.py
@@ -1,12 +1,33 @@
 import matplotlib.pyplot as plt
 from sklearn import datasets
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
 
 digits = datasets.load_digits()
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, label in zip(axes, digits.images, digits.target):
-    ax.set_axis_off()
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-    ax.set_title('Mostra del valor: %i' % label)
 
-plt.show()
+#Preprocessament
+digits2 = []
+for d in digits.images:
+    digits2.append(np.reshape(d, 64))
+
+X_train, X_test, y_train, y_test = train_test_split(digits2, digits.target, test_size = 0.3, random_state = 5)
+
+clf = SGDClassifier(loss = "log", eta0 = 0.1, max_iter=1000, learning_rate="constant", random_state = 5)
+
+clf.fit(X_train, y_train)
+prediction = clf.predict(X_test)
+
+#Avaluació
+cf_matrix = confusion_matrix(y_test, prediction)
+print(cf_matrix)
+accuracy = accuracy_score(y_test, prediction)
+print("Accuracy: ", accuracy)
+
+numeros = ["0","1","2","3","4","5","6","7","8","9",]
+print(classification_report(y_test, prediction, target_names = numeros))
