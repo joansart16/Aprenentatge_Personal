32af7536ad382bde5fd53cfcbfed8dd81ac7d679
Joan <joan.sart@estudiant.upc.edu>
2021-12-24 12:02:24 +0100

    Update main.py

--- 06_Parameter_adjustment/main.py
+++ 06_Parameter_adjustment/main.py
@@ -20,8 +20,11 @@ features_names = datos.columns
 
 #Separació de dades: entrenament i test
 
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 5)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 5)
 estimator = RandomForestRegressor()
+
+param_grid = {"estimator": [10,20,40,80,100,120,140,160,200], "min_samples_split":[2,4,8,16], "min_samples_leaf":[1,2,4,8,16,32], "max_features": ["auto", "sqrt", "log2"]}
+
 estimator.fit(X,y)
 print(estimator.get_params())
 grid = GridSearchCV(estimator, features_names, cv=None, verbose=0)
